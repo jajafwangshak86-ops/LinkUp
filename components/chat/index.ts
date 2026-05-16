f2300dad44b75e68714bd72d53baea632eb4ad77
LinkUp Dev <dev@linkup.app>
2026-05-16 10:07:56 -0400

    chore(components/chat): export ViewOnceMessage and ImageMessage from index

--- components/chat/index.ts
+++ components/chat/index.ts
@@ -2,3 +2,5 @@ export { ChatCard } from './ChatCard';
 export { MessageBubble } from './MessageBubble';
 export { MessageInput } from './MessageInput';
 export { PaymentMessage } from './PaymentMessage';
+export { ViewOnceMessage } from './ViewOnceMessage';
+export { ImageMessage } from './ImageMessage';
