891ce88e05587104582b4c91e49565f20362b1f9
LinkUp Dev <dev@linkup.app>
2026-05-16 08:27:52 -0400

    chore(components/wallet): export StxPriceCard and AllowanceBar from index

--- components/wallet/index.ts
+++ components/wallet/index.ts
@@ -1,3 +1,5 @@
 export { TokenCard } from './TokenCard';
 export { TransactionCard } from './TransactionCard';
 export { SendForm } from './SendForm';
+export { StxPriceCard } from './StxPriceCard';
+export { AllowanceBar } from './AllowanceBar';
